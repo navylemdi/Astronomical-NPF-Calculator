db7986bdb399d45f02a47271fe2850d73a142e3a
DILEM Yvan <yvan.dilem.1@ens.etsmtl.ca>
2021-09-02 17:43:06 -0400

    first commit

---
+++ Astronomical NPF Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Astronomical NPF Calculator
+//
+//  Created by DILEM Yvan on 01/09/2021.
+//  Copyright © 2021 BibouCorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
