3820d44d25faee2abd979438601c7495ebb205e8
DILEM Yvan <yvan.dilem.1@ens.etsmtl.ca>
2021-09-03 10:25:55 -0400

    Changes

--- Astronomical NPF Calculator/ViewController.swift
+++ Astronomical NPF Calculator/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var ApertureField: UITextField!
+    @IBOutlet weak var FocalLengthField: UITextField!
+    @IBOutlet weak var NbpixelField: UITextField!
+    @IBOutlet weak var PixDimField: UITextField!
+    @IBOutlet weak var DeclinationField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.ApertureField.delegate = self
+        self.FocalLengthField.delegate = self
+        self.NbpixelField.delegate = self
+        self.PixDimField.delegate = self
+        self.DeclinationField.delegate = self
     }
-
-
+    func hidekeyboard(){
+        ApertureField.resignFirstResponder()
+        FocalLengthField.resignFirstResponder()
+        NbpixelField.resignFirstResponder()
+        PixDimField.resignFirstResponder()
+        DeclinationField.resignFirstResponder()
+    }
+    
+//    UITextFieldDelegate Methods
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("Return pressed")
+        hidekeyboard()
+        return true
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 }
-
